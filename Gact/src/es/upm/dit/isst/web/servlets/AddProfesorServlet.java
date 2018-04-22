fa6418b7d4a4b918e11762c962a28a592ed8f47b
elenaCORE <elenaCORE@isst>
2018-04-22 18:51:52 +0200

    Añadir profesor incompleto

--- Gact/src/es/upm/dit/isst/web/servlets/AddProfesorServlet.java
+++ Gact/src/es/upm/dit/isst/web/servlets/AddProfesorServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AddProfesorServlet extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException { 
-		
+		//comentario
 		
 		
 	}
